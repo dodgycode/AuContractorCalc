71a12c7394ed2484d363484d364b2ada45f52f05
Owen Pyrah <owenpyrah@hotmail.com>
2024-11-08 11:42:47 +0800

    Added toml

--- ContractorTakehomeCalc/src/App.vue
+++ ContractorTakehomeCalc/src/App.vue
@@ -3,9 +3,9 @@ import Calc from './components/Calc.vue';
 </script>
 
 <template>
-  <header>
+  <!-- <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-  </header>
+  </header> -->
 
   <main>
     <Calc/>
